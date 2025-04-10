dee64f191e4470dd420d55d4625c5415ef7f8e83
BoyceYi <1473416941@qq.com>
2025-04-10 11:21:37 +0800

    Revise

--- models/Compen.py
+++ models/Compen.py
@@ -1,7 +1,6 @@
 import torch
 from compressai.models.google import ScaleHyperprior
 from utils.STE import LearnableBiasModule, quantize_ste_biased, quantize_ste
-from utils.entropy_models import GaussianConditionalBiased
 
 
 class Compensation(ScaleHyperprior):
@@ -32,7 +31,6 @@ class CompensationWithBias(ScaleHyperprior):
     def __init__(self, N, M, bias_type='tanh',*args, **kwargs):
         super().__init__(N=N, M=M, **kwargs)
         self.bias = LearnableBiasModule(init_val=0.0, init_type=bias_type)
-        self.gaussian_conditional = GaussianConditionalBiased(bias=self.bias)
 
     def forward(self, input):
         half = float(0.5)
@@ -41,7 +39,7 @@ class CompensationWithBias(ScaleHyperprior):
         z_hat, z_likelihoods = self.entropy_bottleneck(z)
         scales_hat = self.h_s(z_hat)
         y_hat = quantize_ste_biased(y, bias=self.bias)
-        _, y_likelihoods = self.gaussian_conditional(y, scales_hat, bias=self.bias)
+        _, y_likelihoods = self.gaussian_conditional(y, scales_hat)
         y_hat = y_hat + torch.empty_like(y_hat).normal_(mean=0, std=1/6).clamp_(-half, half)
         x_hat = self.g_s(y_hat)
 
